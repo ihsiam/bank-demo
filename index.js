066b4e1c5077905ab5af119a01a2e7dddeb9eedf
Md Ismile Hosen Siam <110395673+ihsiam@users.noreply.github.com>
2023-02-21 16:30:19 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,46 +2,44 @@
 
 const submit = document.getElementById("sbmt");
 const hme = document.getElementById("home");
-const trans = document.getElementById("tran");
-trans.style.display = "none";
+const Transaction = document.getElementById("transaction");
+Transaction.style.display = "none";
 
 submit.addEventListener('click', function(){
     hme.style.display = "none";
-    trans.style.display = "block";
+    Transaction.style.display = "block";
 });
-
-
 
 //deposit
 
 const depbtn = document.getElementById("add");
 
-
 depbtn.addEventListener('click',function(){
-    const amntDep = parseFloat(document.getElementById("dep").value);
-    const totalDep = parseFloat(document.getElementById("tdep").innerText);
-    const tBalance = parseFloat(document.getElementById("tblnc").innerText);
 
-    const totalDeposit = amntDep + totalDep;
-    const totalBalance = tBalance + amntDep;
-    document.getElementById("tdep").innerText = totalDeposit;
-    document.getElementById("tblnc").innerText = totalBalance;
+    const amntDep = parseFloat(document.getElementById("dep-Amount").value);
+
+    updateBalance("crnt-dep",amntDep);
+    updateBalance("crnt-blnc",amntDep);
+    document.getElementById("dep-Amount").value = "";
 });
-
-
 
 //withdraw 
 
 const wthbtn = document.getElementById("remove");
 
-
 wthbtn.addEventListener('click',function(){
-    const amntWith = parseFloat(document.getElementById("with").value);
-    const totalwth = parseFloat(document.getElementById("twith").innerText);
-    const tBalance = parseFloat(document.getElementById("tblnc").innerText);
 
-    const totatWithdraw = amntWith + totalwth;
-    const totalBalance = tBalance - amntWith;
-    document.getElementById("twith").innerText = totatWithdraw;
-    document.getElementById("tblnc").innerText = totalBalance;
+    const amntWith = parseFloat(document.getElementById("with-Amount").value);
+   
+    updateBalance("crnt-with",amntWith);
+    updateBalance("crnt-blnc",-1*amntWith);
+    document.getElementById("with-Amount").value = "";
 });
+
+//function 
+
+function updateBalance(id, amnt){
+    const currentBalance = parseFloat(document.getElementById(id).innerText);
+    const totalBalance = currentBalance + amnt;
+    document.getElementById(id).innerText = totalBalance;
+}
